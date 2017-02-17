e011e04c14806cb83008eda8454adffa1960d6a8
student <burnetealexandra@yahoo.com>
2017-02-17 12:13:11 +0200

    SelectMany

---
+++ LinqPractice/SelectMany.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+	public static partial class Enumerable
+	{
+		public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, IEnumerable<TResult>> selector)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source is null");
+			}
+			if (selector == null)
+			{
+				throw new ArgumentNullException("selector is null");
+			}
+			return SelectManyImpl(source, selector);
+		}
+		private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(
+			IEnumerable<TSource> source,
+			Func<TSource, IEnumerable<TResult>> selector)
+		{
+			foreach (TSource item in source)
+			{
+				foreach (TResult result in selector(item))
+				{
+					yield return result;
+				}
+			}
+		}
+
+		public static IEnumerable<TResult> SelectMany<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, int, IEnumerable<TResult>> selector)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source is null");
+			}
+			if (selector == null)
+			{
+				throw new ArgumentNullException("selector is null");
+			}
+			return SelectManyImpl(source, selector);
+		}
+
+		private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(
+			IEnumerable<TSource> source,
+			Func<TSource, int, IEnumerable<TResult>> selector)
+		{
+			int index = 0;
+			foreach (TSource item in source)
+			{
+				foreach (TResult result in selector(item, index++))
+				{
+					yield return result;
+				}
+			}
+		}
+
+		public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, IEnumerable<TCollection>> collectionSelector,
+			Func<TSource, TCollection, TResult> resultSelector)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (collectionSelector == null)
+			{
+				throw new ArgumentNullException("collectionSelector");
+			}
+			if (resultSelector == null)
+			{
+				throw new ArgumentNullException("resultSelector");
+			}
+			return SelectManyImpl(source, collectionSelector, resultSelector);
+		}
+
+		private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(
+		   IEnumerable<TSource> source,
+		   Func<TSource, IEnumerable<TCollection>> collectionSelector,
+		   Func<TSource, TCollection, TResult> resultSelector)
+		{
+			foreach (TSource item in source)
+			{
+				foreach (TCollection collectionItem in collectionSelector(item))
+				{
+					yield return resultSelector(item, collectionItem);
+				}
+			}
+		}
+		public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+			Func<TSource, TCollection, TResult> resultSelector)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			if (collectionSelector == null)
+			{
+				throw new ArgumentNullException("collectionSelector");
+			}
+			if (resultSelector == null)
+			{
+				throw new ArgumentNullException("resultSelector");
+			}
+			return SelectManyImpl(source, collectionSelector, resultSelector);
+		}
+
+		private static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(
+			IEnumerable<TSource> source,
+			Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+			Func<TSource, TCollection, TResult> resultSelector)
+		{
+			int index = 0;
+			foreach (TSource item in source)
+			{
+				foreach (TCollection collectionItem in collectionSelector(item, index++))
+				{
+					yield return resultSelector(item, collectionItem);
+				}
+			}
+
+		}
+	}
+}
---
+++ LinqTests/SelectManyTests.cs
@@ -0,0 +1,57 @@
+﻿using LinqPractice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTests
+{
+	[TestFixture]
+	class SelectManyTests
+	{
+
+		[Test]
+		public void FlattenWithProjectionAndIndex()
+		{
+			int[] numbers = { 3, 5, 20, 15 };
+			var query = numbers.SelectMany((x, index) => (x + index).ToString().ToCharArray(),
+				(x, c) => x + ": " + c);
+			// 3 => 3:3
+			// 5 => 5:6
+			// 20 => 20:2 , 20:2 (20+2=22)-> 20:2, 20:2
+			// 15 => 15:1, 15:8 (15+3 =18)-> 15:1, 15:8
+			query.AssertSequenceEqual("3: 3", "5: 6", "20: 2", "20: 2", "15: 1", "15: 8");
+		}
+
+		[Test]
+		public void NullSourceThrowsNullArgumentException()
+		{
+			int[] numbers = null;
+			Assert.Throws<ArgumentNullException>(() => numbers.SelectMany(x => x.ToString()));
+		}
+
+		[Test]
+		public void SimpleFlatten()
+		{
+			string[] strings = { "aA", "B", "b" };
+			var query = strings.SelectMany(x => x.ToString().ToLower());
+			query.AssertSequenceEqual('a', 'a', 'b', 'b');
+		}
+
+		[Test]
+		public void FlattenWithIndex()
+		{
+			int[] numbers = { 3, 5, 20 };
+			var query = numbers.SelectMany((x, index) => (x + index).ToString().ToCharArray());
+			query.AssertSequenceEqual('3', '6', '2', '2');
+		}
+		[Test]
+		public void FlattenWithProjection()
+		{
+			int[] numbers = { 1, 24 };
+			var query = numbers.SelectMany(x => x.ToString().ToCharArray(), (x, c) => x + ": " + c);
+			query.AssertSequenceEqual("1: 1", "24: 2", "24: 4");
+		}
+	}
+}
