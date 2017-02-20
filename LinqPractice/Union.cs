7a9322755463b3878e75cdacc98b8cf0b695a375
student <burnetealexandra@yahoo.com>
2017-02-20 14:37:59 +0200

    Union

---
+++ LinqPractice/Union.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+	public static partial  class Enumerable
+	{
+		public static IEnumerable<TSource> Union<TSource>(
+			this IEnumerable<TSource> first,
+			IEnumerable<TSource> second)
+		{
+			return Union(first, second, EqualityComparer<TSource>.Default);
+		}
+		public static IEnumerable<TSource> Union<TSource>(
+			this IEnumerable<TSource> first,
+			IEnumerable<TSource> second,
+			IEqualityComparer<TSource> comparer)
+		{
+			if(first == null)
+			{
+				throw new ArgumentNullException("first is null");
+			}
+			if(second == null)
+			{
+				throw new ArgumentNullException("second is null");
+			}
+			return UnionImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+		}
+
+		private static IEnumerable<TSource> UnionImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second,IEqualityComparer<TSource> comparer)
+		{
+			HashSet < TSource > seenElements= new HashSet<TSource>(comparer);
+			foreach (TSource item in first)
+			{
+				if (seenElements.Add(item))
+				{
+					yield return item;
+				}
+			}
+			foreach (TSource item in second)
+			{
+				if (seenElements.Add(item))
+				{
+					yield return item;
+				}
+			}
+		}
+	}
+}
---
+++ LinqTests/UnionTests.cs
@@ -0,0 +1,111 @@
+﻿using LinqPractice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTests
+{
+	[TestFixture]
+	class UnionTests
+	{
+		[Test]
+		public void NullFirstWithoutComparer()
+		{
+			string[] first = null;
+			string[] second = { };
+			Assert.Throws<ArgumentNullException>(() => first.Union(second));
+		}
+		[Test]
+		public void NullSecondWithoutComparer()
+		{
+			string[] first = { };
+			string[] second = null;
+			Assert.Throws<ArgumentNullException>(() => first.Union(second));
+		}
+		[Test]
+		public void NullFirstWithComparer()
+		{
+			string[] first = null;
+			string[] second = { };
+			Assert.Throws<ArgumentNullException>(() => first.Union(second, StringComparer.Ordinal));
+		}
+		[Test]
+		public void NullSecondWithComparer()
+		{
+			string[] first = { };
+			string[] second = null;
+			Assert.Throws<ArgumentNullException>(() => first.Union(second, StringComparer.Ordinal));
+		}
+		[Test]
+		public void UnionWithoutComparer()
+		{
+			string[] first = { "a", "b", "B", "c", "b" };
+			string[] second = { "d", "e", "d", "a" };
+			first.Union(second).AssertSequenceEqual("a", "b", "B", "c", "d", "e");
+		}
+		[Test]
+		public void UnionWithComparerNull()
+		{
+			string[] first = { "a", "b", "B", "c", "b" };
+			string[] second = { "d", "e", "d", "a" };
+			first.Union(second, null).AssertSequenceEqual("a", "b", "B", "c", "d", "e");
+		}
+		[Test]
+		public void UnionWithCaseInsensitiveComparer()
+		{
+			string[] first = { "a", "b", "B", "c", "b" };
+			string[] second = { "d", "e", "d", "a" };
+			first.Union(second, StringComparer.OrdinalIgnoreCase).AssertSequenceEqual("a", "b", "c", "d", "e");
+		}
+		[Test]
+		public void UnionWithEmptyFirstSequence()
+		{
+			string[] first = { };
+			string[] second = { "d", "e", "d" };
+			first.Union(second).AssertSequenceEqual("d", "e");
+		}
+		[Test]
+		public void UnionWithEmptySecondSequence()
+		{
+			string[] first = { "a", "b", "B", "c", "b" };
+			string[] second = { };
+			first.Union(second).AssertSequenceEqual("a", "b", "B", "c");
+		}
+
+		[Test]
+		public void UnionWithTwoEmptySequences()
+		{
+			string[] first = { };
+			string[] second = { };
+			first.Union(second).AssertSequenceEqual();
+		}
+		[Test]
+		public void FirstSequenceIsNotUsedUntilQueryIsIterated()
+		{
+			var first = new ThrowingEnumerable();
+			int[] second = { 2 };
+			var query = first.Union(second);
+			using (var iterator = query.GetEnumerator())
+			{
+				Assert.Throws<InvalidOperationException>(() => iterator.MoveNext());
+			}
+		}
+		[Test]
+		public void SecondSequenceIsNotUSedUntilFirstIsExhausted()
+		{
+			int[] first = { 3, 5, 3 };
+			var second = new ThrowingEnumerable();
+			using (var iterator = first.Union(second).GetEnumerator())
+			{
+				Assert.IsTrue(iterator.MoveNext());
+				Assert.AreEqual(3, iterator.Current);
+				Assert.IsTrue(iterator.MoveNext());
+				Assert.AreEqual(5, iterator.Current);
+
+				Assert.Throws<InvalidOperationException>(() => iterator.MoveNext());
+			}
+		}
+	}
+}
