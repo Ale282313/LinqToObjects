df2608e586e0efcd4172e487c5a4b1f4f2f3f71a
student <burnetealexandra@yahoo.com>
2017-02-20 16:21:57 +0200

    Except

---
+++ LinqPractice/Except.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+	public static partial class Enumerable
+	{
+		public static IEnumerable<TSource> Except<TSource>(
+		   this IEnumerable<TSource> first,
+		   IEnumerable<TSource> second)
+		{
+			return Except(first, second, EqualityComparer<TSource>.Default);
+		}
+
+		public static IEnumerable<TSource> Except<TSource>(
+			this IEnumerable<TSource> first,
+			IEnumerable<TSource> second,
+			IEqualityComparer<TSource> comparer)
+		{
+			if (first == null)
+			{
+				throw new ArgumentNullException("first");
+			}
+			if (second == null)
+			{
+				throw new ArgumentNullException("second");
+			}
+			return ExceptImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+		}
+		private static IEnumerable<TSource> ExceptImpl<TSource>(
+			IEnumerable<TSource> first,
+			IEnumerable<TSource> second,
+			IEqualityComparer<TSource> comparer)
+		{
+			HashSet<TSource> bannedElements = new HashSet<TSource>(second, comparer);
+			foreach (TSource item in first)
+			{
+				if (bannedElements.Add(item))
+				{
+					yield return item;
+				}
+			}
+		}
+	}
+}
