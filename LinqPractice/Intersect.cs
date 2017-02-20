271a06afd8a668652f969841a9a9d00b5f152d91
student <burnetealexandra@yahoo.com>
2017-02-20 16:05:44 +0200

    Intersect

---
+++ LinqPractice/Intersect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+	public static partial class Enumerable
+	{
+		public static IEnumerable<TSource> Intersect<TSource>(
+			this IEnumerable<TSource> first,
+			IEnumerable<TSource> second)
+		{
+			return Intersect(first, second, EqualityComparer<TSource>.Default);
+		}
+		public static IEnumerable<TSource> Intersect<TSource>(
+			this IEnumerable<TSource> first,
+			IEnumerable<TSource> second,
+			IEqualityComparer<TSource> comparer)
+		{
+			if( first == null)
+			{
+				throw new ArgumentNullException("first is null");
+			}
+			if( second == null)
+			{
+				throw new ArgumentNullException("second id null");
+			}
+			return IntersectImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+		}
+		private static IEnumerable<TSource> IntersectImpl<TSource>(
+			IEnumerable<TSource> first,
+			IEnumerable<TSource> second,
+			IEqualityComparer<TSource> comparer)
+		{
+			HashSet<TSource> potentialElements = new HashSet<TSource>(second, comparer);
+			foreach (TSource item in first)
+			{
+				if (potentialElements.Remove(item))
+				{
+					yield return item;
+				}
+			}
+		}
+	}
+}
---
+++ LinqTests/IntersectTests.cs
@@ -0,0 +1,99 @@
+﻿using LinqPractice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTests
+{
+	[TestFixture]
+	class IntersectTests
+	{
+		[Test]
+		public void NullFirstWithoutComparer()
+		{
+			string[] first = null;
+			string[] second = { };
+			Assert.Throws<ArgumentNullException>(() => first.Intersect(second));
+		}
+		[Test]
+		public void NullSecondWithoutComparer()
+		{
+			string[] first = { };
+			string[] second = null;
+			Assert.Throws<ArgumentNullException>(() => first.Intersect(second));
+		}
+		[Test]
+		public void NullFirstWithComparer()
+		{
+			string[] first = null;
+			string[] second = { };
+			Assert.Throws<ArgumentNullException>(() => first.Intersect(second, StringComparer.Ordinal));
+		}
+		[Test]
+		public void NullSecondWithComparer()
+		{
+			string[] first = { };
+			string[] second = null;
+			Assert.Throws<ArgumentNullException>(() => first.Intersect(second, StringComparer.Ordinal));
+		}
+		[Test]
+		public void NoComparerSpecified()
+		{
+			string[] first = { "A", "a", "b", "c", "b" };
+			string[] second = { "b", "a", "d", "a" };
+			first.Intersect(second).AssertSequenceEqual("a", "b");
+		}
+		[Test]
+		public void NullComparerSpecified()
+		{
+			string[] first = { "A", "a", "b", "c", "b" };
+			string[] second = { "b", "a", "d", "a" };
+			first.Intersect(second, null).AssertSequenceEqual("a", "b");
+		}
+		[Test]
+		public void CaseInsensitiveComparerSpecified()
+		{
+			string[] first = { "A", "a", "b", "c", "b" };
+			string[] second = { "b", "a", "d", "a" };
+			first.Intersect(second, StringComparer.OrdinalIgnoreCase).AssertSequenceEqual("A", "b");
+		}
+		[Test]
+		public void NoSequenceUsedBeforeIteration()
+		{
+			var first = new ThrowingEnumerable();
+			var second = new ThrowingEnumerable();
+			var query = first.Intersect(second);
+			using (var iterator = query.GetEnumerator())
+			{ }
+		}
+		[Test]
+		public void SecodSequenceReadFullyOnFirstResultIteration()
+		{
+			int[] first = { 1 };
+			var secondQuery = new[] { 10, 2, 0 }.Select(x => 10 / x);
+
+			var query = first.Intersect(secondQuery);
+			using (var iterator = query.GetEnumerator())
+			{
+				Assert.Throws<DivideByZeroException>(() => iterator.MoveNext());
+			}
+		}
+		[Test]
+		public void FirstSequenceOnlyReadAsResultsAreRead()
+		{
+			var firstQuery = new[] { 10, 2, 0, 2 }.Select(x => 10 / x);
+			int[] second = { 1 };
+			var query = firstQuery.Intersect(second);
+			using( var iterator = query.GetEnumerator())
+			{
+				Assert.IsTrue(iterator.MoveNext());
+				Assert.AreEqual(1, iterator.Current);
+				// getting second result requires reading from the input sequence until the 0 division
+				Assert.Throws<DivideByZeroException>(() => iterator.MoveNext());
+			}
+		}
+	}
+}
