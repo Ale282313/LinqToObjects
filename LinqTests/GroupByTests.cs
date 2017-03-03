3efdd294bb8e3fc2eec1a44051be354f1284b13b
student <burnetealexandra@yahoo.com>
2017-03-03 15:57:19 +0200

    GroupBy

--- LinqTests/GroupByTests.cs
+++ LinqTests/GroupByTests.cs
@@ -1,7 +1,7 @@
-﻿using NUnit.Framework;
+﻿using LinqPractice;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
