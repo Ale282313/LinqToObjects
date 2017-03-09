8752572bd7183539903b1c7eeeefc34b56b8ab58
student <burnetealexandra@yahoo.com>
2017-03-09 17:55:35 +0200

    ToArray

---
+++ LinqPractice/ToArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqPractice
+{
+	public static partial class Enumerable
+	{
+		public static TSource[] ToArray<TSource>(this IEnumerable<TSource> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException("source");
+			}
+			ICollection<TSource> collection = source as ICollection<TSource>;
+			if (collection != null)
+			{
+				TSource[] ret = new TSource[collection.Count];
+				collection.CopyTo(ret, 0);
+				return ret;
+			}
+			return new List<TSource>(source).ToArray();
+		}
+	}
+}
---
+++ LinqTests/ToArrayTests.cs
@@ -0,0 +1,54 @@
+﻿using LinqPractice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TestSupport;
+
+namespace LinqTests
+{
+	[TestFixture]
+	class ToArrayTests
+	{
+		[Test]
+		public void resultIsIndependentOfSource()
+		{
+			List<string> source = new List<string>() { "xyz", "abc" };
+
+			string[] result = Enumerable.ToArray(source);
+
+			source[0] = "xxxx";
+			Assert.AreEqual("xyz", result[0]);
+
+			result[1] = "yyy";
+			Assert.AreEqual("abc", source[1]);
+		}
+		[Test]
+		public void sequenceIsEvaluatedEagerly()
+		{
+			int[] numbers = { 5, 3, 0 };
+			var query = numbers.Select(x => 10 / x);
+			Assert.Throws<DivideByZeroException>(() => query.ToArray());
+		}
+		[Test]
+		public void NullSource()
+		{
+			IEnumerable<string> source = null;
+			Assert.Throws<ArgumentNullException>(() => source.ToArray());
+		}
+		[Test]
+		public void ConversionOfLazyEvaluatedSequence()
+		{
+			var range = Enumerable.Range(3, 3);
+			var query = range.Select(x => x * 2);
+			var list = query.ToArray();
+			list.AssertSequenceEqual(6, 8, 10);
+		}
+		[Test]
+		public void IcollectionOptimization()
+		{
+			var source = new NonEnumerableCollection<string> { "hello", "there" };
+			var list = source.ToArray();
+			list.AssertSequenceEqual("hello", "there");
+		}
+	}
+}
